ba3f6f8bd3a7c2adcc922fbbd57ce7d1b4e0dc4c
Vinicius@dell <vinicius.darocha@das.ufsc.br>
2013-07-21 19:35:06 -0300

    first version of image-server: echo posted image

--- project.clj
+++ project.clj
@@ -2,8 +2,12 @@
   :description "This is a small clojure server to store and retrieve images"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.5"]]
-  :plugins [[lein-ring "0.8.5"]]
-  :ring {:handler image-server.handler/app}
+                 [compojure "1.1.5"]
+                 [hiccup "1.0.3"]
+                 [ring "1.2.0"]]
+  :plugins [[lein-ring "0.8.6"]]
+  :ring {:handler image-server.handler/app
+         :port 8080}
   :profiles
-  {:dev {:dependencies [[ring-mock "0.1.5"]]}})
+  {:dev {:dependencies [[ring-mock "0.1.5"]
+                        #_[ring-serve "0.1.2"]]}})
--- src/image_server/handler.clj
+++ src/image_server/handler.clj
@@ -1,12 +1,59 @@
 (ns image-server.handler
   (:use compojure.core)
+  (:use hiccup.core
+        hiccup.form
+        [ring.adapter.jetty :only (run-jetty)]
+        ring.middleware.stacktrace
+        ring.middleware.multipart-params)
+  (:require [clojure.java.io :as io])
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route])
+  (:import [java.io ByteArrayOutputStream ByteArrayInputStream InputStream File])
+  #_(:use ring.util.serve))
+
+(defn temp-file-store [item]
+  (let [temp-file (File. "/tmp/tmpfile")]
+    (io/copy (:stream item) temp-file)
+    (-> (select-keys item [:filename :content-type])
+        (assoc :tempfile temp-file
+               :size (.length temp-file)))))
+
+(defn buff-file-store [item]
+  (println "Using buff-file-store")
+  (let [out (ByteArrayOutputStream.)]
+    (io/copy (:stream item) out)
+    (io/copy (:stream item) (File. "resources/asd.jpg"))
+    (-> (select-keys item [:filename :content-type])
+        (assoc :byte-array (.toByteArray out)))))
+
+(def form
+  (html
+   [:head
+    [:title
+     "File upload server"]]
+   [:html
+    (form-to {:enctype "multipart/form-data"} [:post "/upload-file"]
+             "Chose file to process" [:br]
+             (file-upload "filename")
+             [:br]
+             (submit-button "Send!"))]))
+
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] form)
+  (POST "/upload-file" req (do
+                             #_(println req)
+                             (ByteArrayInputStream. (-> req :params :filename :byte-array))))
   (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app
-  (handler/site app-routes))
+  (-> (handler/api app-routes)
+      (wrap-multipart-params {:store buff-file-store})
+      wrap-stacktrace-web))
+
+(def server (atom nil))
+
+(defn start-server []
+  (when (not @server)
+    (reset! server (run-jetty #'app {:port 8080 :join? false}))))
